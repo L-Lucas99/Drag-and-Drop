715d05bb89d1d23c08b1e60b4a35f40be44f27de
L-Lucas99 <l.contreras17@alumnos.santotomas.cl>
2021-08-29 16:30:07 -1200

    Comentarios agregados

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-function dragStart(ev) {
-   /*  ev.dataTransfer.effectAllowed='move'; */
+//Evento asignado al elemento que se desea poder arrastrar y mover
+const dragStart = (ev) => {
+   /*ev.dataTransfer.effectAllowed='move'; */
     ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
-    /* ev.dataTransfer.setDragImage(ev.target,0,0); */
+    /*ev.dataTransfer.setDragImage(ev.target,0,0); */
     return true;
  }
- function dragEnter(ev) {
+
+//Se activa cuando el elemento arrastrado 
+//entra en una zona permitida para dejar el elemento
+const dragEnter = (ev) => {
     ev.preventDefault();
     return true;
  }
 
-
+ //Se activa mientras el elemento arrastrado se 
+ //matenga sobre la zona permitida para dejar el elemento
 const dragOver = (ev) =>  {
     return false;
  }
- function dragDrop(ev) {
+
+ //Se activa cuando el elemento arrastrado se 
+ //haya dejado en la zona permitida para dejar el elemento
+const dragDrop = (ev) => {
     var src = ev.dataTransfer.getData("Text");
     ev.target.appendChild(document.getElementById(src));
     ev.stopPropagation();
